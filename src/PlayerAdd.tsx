f9b8ca03a0235c7f007d040b2e620932cfb6cdcd
BJ Clark <bjclark13@outlook.com>
2021-11-11 21:10:29 -0500

    Add submit form

--- src/PlayerAdd.tsx
+++ src/PlayerAdd.tsx
@@ -1,33 +1,49 @@
 import IPlayer from "./IPlayer";
+import { useState } from 'react';
 
 function PlayerAdd({ onAdd }: { onAdd: (player: IPlayer) => void }) {
+    const [name, setName] = useState('');
+    const [position, setPosition] = useState('');
+    const [starting, setStarting] = useState(false);
+
     return (
-        <form className="player">
+        <form className="player" onSubmit={(e) => {
+            e.preventDefault();
+            onAdd({ name, starting, position })
+        }}>
             <p>
                 <label>
                     <strong> Name </strong>
-                    <input name="name" />
+                    <input value={name} onChange={e => setName(e.target.value)} name="name" />
                 </label>
             </p>
 
             <p>
                 <label>
                     <strong> Position </strong>
-                    <input name="position" />
+                    <input
+                        value={position}
+                        onChange={e => setPosition(e.target.value)}
+                        name="position"
+                    />
                 </label>
             </p>
 
             <p>
                 <label>
                     <strong> Start Player Upon Adding? </strong>
-                    <input name="starting" type="checkbox" />
+                    <input
+                        name="starting"
+                        type="checkbox"
+                        checked={starting}
+                        onChange={e => setStarting(e.target.checked)}
+                    />
                 </label>
             </p>
 
             <p>
                 <button> Submit </button>
             </p>
-
         </form>
     )
 };
