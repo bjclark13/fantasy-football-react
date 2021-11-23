0b3d738cff374801a48c7b24a74fc9cc9e4cda47
BJ Clark <bjclark13@outlook.com>
2021-11-22 20:19:02 -0500

    add the final code

--- src/PlayerBio.tsx
+++ src/PlayerBio.tsx
@@ -1,7 +1,25 @@
+import { useParams } from "react-router";
+import { useContext } from 'react';
+import PlayersContext from "./PlayersContext";
+import { Link } from 'react-router-dom';
+
 function PlayerBio() {
+    const playerName = useParams().name;
+
+    const { players } = useContext(PlayersContext);
+
+    // find the player where the name matches player name
+    const player = players.find((player) => {
+        return player.name === playerName
+    })
+
+
     return (
         <div>
-            PLAYER BIO INFORMATION HERE
+            <h1>{playerName}</h1>
+
+            <h2>{player?.position}</h2>
+            <Link to="/">Go Back Home</Link>
         </div>
     )
 }
